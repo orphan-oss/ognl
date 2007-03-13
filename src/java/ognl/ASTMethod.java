846cf16b3d4578841bad081530f582d3f8bc04b7
Jesse Kuhnert <jkuhnert@gmail.com>
2007-03-13 00:10:16 +0000

    Slight code cleanup.

--- src/java/ognl/ASTMethod.java
+++ src/java/ognl/ASTMethod.java
@@ -240,7 +240,6 @@ public class ASTMethod extends SimpleNode implements OrderedReturn, NodeType
                             + wrapClass.getName() + ".class, true))."
                             + OgnlRuntime.getNumericValueGetter(wrapClass);
                         } else if (parms[i] != Object.class) {
-                            
                             parmString = "(" + parms[i].getName() + ")ognl.OgnlOps.convertValue(" + parmString + "," + parms[i].getName() + ".class)";
                         } else if ((NodeType.class.isInstance(_children[i])
                                 && ((NodeType)_children[i]).getGetterClass() != null 
