e47448062f181e1763756acc84aa37928f7f5139
Jesse Kuhnert <jkuhnert@gmail.com>
2007-07-06 17:53:25 +0000

    Added in MVEL comparisons in performance tests.

--- src/test/java/org/ognl/test/Performance.java
+++ src/test/java/org/ognl/test/Performance.java
@@ -34,8 +34,10 @@ import ognl.Ognl;
 import ognl.OgnlContext;
 import ognl.OgnlException;
 import ognl.SimpleNode;
+import org.mvel.MVEL;
 import org.ognl.test.objects.Bean1;
 
+import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -49,65 +51,70 @@ public class Performance extends Object
     private static boolean TIME_MODE;
     private static NumberFormat FACTOR_FORMAT = new DecimalFormat("0.000");
 
-    private String name;
-    private OgnlContext context = (OgnlContext) Ognl.createDefaultContext(null);
-    private Bean1 root = new Bean1();
-    private SimpleNode expression;
-    private SimpleNode compiledExpression;
-    private Method method;
-    private int iterations;
+    private String _name;
+    private OgnlContext _context = (OgnlContext) Ognl.createDefaultContext(null);
+    private Bean1 _root = new Bean1();
+    private SimpleNode _expression;
+    private SimpleNode _compiledExpression;
+    private Method _method;
+    private int _iterations;
+    private Serializable _mvelCompiled;
+    private String _expressionString;
+    private boolean _isMvel = false;
     private long t0;
     private long t1;
-    
+
     /*
-     * =================================================================== Private static classes
-     * ===================================================================
-     */
+    * =================================================================== Private static classes
+    * ===================================================================
+    */
     private static class Results
     {
 
         int iterations;
         long time;
+        boolean mvel;
 
-        public Results(int iterations, long time)
+        public Results(int iterations, long time, boolean mvel)
         {
             super();
             this.iterations = iterations;
             this.time = time;
+            this.mvel = mvel;
         }
-        
+
         public String getFactor(Results otherResults)
         {
             String ret = null;
-            
+
             if (TIME_MODE) {
                 float factor = 0;
-                
+
                 if (iterations < otherResults.iterations) {
                     factor = Math.max((float) otherResults.iterations, (float) iterations)
-                    / Math.min((float) otherResults.iterations, (float) iterations);
+                             / Math.min((float) otherResults.iterations, (float) iterations);
                 } else {
                     factor = Math.min((float) otherResults.iterations, (float) iterations)
-                    / Math.max((float) otherResults.iterations, (float) iterations);
+                             / Math.max((float) otherResults.iterations, (float) iterations);
                 }
-                
+
                 ret = FACTOR_FORMAT.format(factor);
                 if (iterations > otherResults.iterations)
                     ret += " times faster than ";
                 else
                     ret += " times slower than ";
-                
+
             } else {
                 float factor = Math.max((float) otherResults.time, (float) time)
-                / Math.min((float) otherResults.time, (float) time);
-                
+                               / Math.min((float) otherResults.time, (float) time);
+
                 ret = FACTOR_FORMAT.format(factor);
                 if (time < otherResults.time)
                     ret += " times faster than ";
                 else
                     ret += " times slower than ";
             }
-            
+
             return ret;
         }
     }
@@ -131,112 +138,133 @@ public class Performance extends Object
             TIME_MODE = true;
             MAX_TIME = 1500;
         }
-        
+
         try {
             Performance[] tests = new Performance[] {
                     new Performance("Constant", "100 + 20 * 5", "testConstantExpression"),
+                    new Performance("Constant", "100 + 20 * 5", "testConstantExpression", true),
                     new Performance("Single Property", "bean2", "testSinglePropertyExpression"),
+                    new Performance("Single Property", "bean2", "testSinglePropertyExpression", true),
                     new Performance("Property Navigation", "bean2.bean3.value", "testPropertyNavigationExpression"),
-                    new Performance("Property Setting with context key", "bean2.bean3.nullValue", "testPropertyNavigationSetting"),
+                    new Performance("Property Navigation", "bean2.bean3.value", "testPropertyNavigationExpression", true),
+                    /*new Performance("Property Setting with context key", "bean2.bean3.nullValue", "testPropertyNavigationSetting"),
+                    new Performance("Property Setting with context key", "bean2.bean3.nullValue", "testPropertyNavigationSetting", true), */                    
                     new Performance("Property Navigation and Comparison", "bean2.bean3.value <= 24",
-                    "testPropertyNavigationAndComparisonExpression"),
+                                    "testPropertyNavigationAndComparisonExpression"),
+                    new Performance("Property Navigation and Comparison", "bean2.bean3.value <= 24",
+                                    "testPropertyNavigationAndComparisonExpression", true),
+                    /* new Performance("Property Navigation with Indexed Access", "bean2.bean3.indexedValue[25]",
+                                    "testIndexedPropertyNavigationExpression"),
                     new Performance("Property Navigation with Indexed Access", "bean2.bean3.indexedValue[25]",
-                    "testIndexedPropertyNavigationExpression"),
+                                    "testIndexedPropertyNavigationExpression", true), */
                     new Performance("Property Navigation with Map Access", "bean2.bean3.map['foo']",
-                    "testPropertyNavigationWithMapExpression"),
+                                    "testPropertyNavigationWithMapExpression"),
+                    new Performance("Property Navigation with Map Access", "bean2.bean3.map['foo']",
+                                    "testPropertyNavigationWithMapExpression", true),
+                    /* new Performance("Property Navigation with Map value set", "bean2.bean3.map['foo']",
+                                    "testPropertyNavigationWithMapSetting"),
                     new Performance("Property Navigation with Map value set", "bean2.bean3.map['foo']",
-                    "testPropertyNavigationWithMapSetting")
+                                    "testPropertyNavigationWithMapSetting", true) */
             };
-            
+
             boolean timeMode = TIME_MODE;
             boolean iterMode = ITERATIONS_MODE;
             long maxTime = MAX_TIME;
             int maxIterations = MAX_ITERATIONS;
-            
-            TIME_MODE = false;
-            ITERATIONS_MODE = true;
-            maxIterations = 1000;
-            
+
+            //TIME_MODE = false;
+            //ITERATIONS_MODE = true;
+            //maxIterations = 1000;
+
             runTests(tests, false);
-            
+
             TIME_MODE = timeMode;
             ITERATIONS_MODE = iterMode;
             MAX_TIME = maxTime;
             MAX_ITERATIONS = maxIterations;
-            
+
             System.out.println("\n\n============================================================================\n");
 
             Thread.sleep(2500);
             runTests(tests, true);
 
             //Thread.sleep(2000);
-            
+
             System.out.println("\n\n============================================================================\n");
             // runTests(tests);
-            
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
+
     static void runTests(Performance[] tests, boolean output)
-    throws Exception
+            throws Exception
     {
         for(int i = 0; i < tests.length; i++) {
             Performance perf = tests[i];
-            
+
             try {
-                
-                Results javaResults = perf.testJava(), 
-                interpretedResults = perf.testExpression(false), 
-                compiledResults = perf.testExpression(true);
-                
+
+                Results javaResults = perf.testJava(),
+                        interpretedResults = perf.testExpression(false),
+                        compiledResults = perf.testExpression(true);
+
                 if (!output)
                     return;
-                
-                System.out.println(perf.getName() + ": " + perf.getExpression().toString());
+
+                System.out.println((compiledResults.mvel ? "MVEL" : "OGNL") + " " + perf.getName() + ": " + perf.getExpression().toString());
                 System.out.println("       java: " + javaResults.iterations + " iterations in " + javaResults.time + " ms");
-                
+
                 System.out.println("   compiled: " + compiledResults.iterations + " iterations in "
-                                + compiledResults.time + " ms ("
-                                + compiledResults.getFactor(javaResults) + "java)");
-                
+                                   + compiledResults.time + " ms ("
+                                   + compiledResults.getFactor(javaResults) + "java)");
+
                 System.out.println("interpreted: " + interpretedResults.iterations + " iterations in "
-                        + interpretedResults.time + " ms ("
-                        + interpretedResults.getFactor(javaResults) + "java)");
-                
+                                   + interpretedResults.time + " ms ("
+                                   + interpretedResults.getFactor(javaResults) + "java)");
+
                 System.out.println();
-                
+
             } catch (OgnlException ex) {
                 ex.printStackTrace();
             }
         }
     }
-    
+
     /*
-     * =================================================================== Constructors
-     * ===================================================================
-     */
+    * =================================================================== Constructors
+    * ===================================================================
+    */
     public Performance(String name, String expressionString, String javaMethodName)
-        throws Exception
+            throws Exception
     {
-        super();
-        this.name = name;
-        
-        expression = (SimpleNode) Ognl.parseExpression(expressionString);
-        compiledExpression = (SimpleNode) Ognl.compileExpression(context, root, expressionString);
+        this(name, expressionString, javaMethodName, false);
+    }
+
+    public Performance(String name, String expressionString, String javaMethodName, boolean mvel)
+            throws Exception
+    {
+        _name = name;
+        _isMvel = mvel;
+        _expressionString = expressionString;
+
         try {
-            method = getClass().getMethod(javaMethodName, new Class[] {});
+            _method = getClass().getMethod(javaMethodName, new Class[] {});
         } catch (Exception ex) {
             throw new OgnlException("java method not found", ex);
         }
-        
-        Ognl.getValue(expression, context, root);
-        
-        //if (expression.getAccessor() == null)
-          //  _compiler.compileExpression(context, expression, root);
-        
-        context.put("contextValue", "cvalue");
+
+        if (!_isMvel)
+        {
+            _expression = (SimpleNode) Ognl.parseExpression(expressionString);
+            _compiledExpression = (SimpleNode) Ognl.compileExpression(_context, _root, expressionString);
+            Ognl.getValue(_expression, _context, _root);
+            _context.put("contextValue", "cvalue");
+        } else
+        {
+            _mvelCompiled = MVEL.compileExpression(expressionString);
+        }
     }
 
     /*
@@ -245,25 +273,25 @@ public class Performance extends Object
      */
     protected void startTest()
     {
-        iterations = 0;
+        _iterations = 0;
         t0 = t1 = System.currentTimeMillis();
     }
 
     protected Results endTest()
     {
-        return new Results(iterations, t1 - t0);
+        return new Results(_iterations, t1 - t0, _isMvel);
     }
 
     protected boolean done()
     {
-        iterations++;
+        _iterations++;
         t1 = System.currentTimeMillis();
 
         if (TIME_MODE) {
             return (t1 - t0) >= MAX_TIME;
         } else {
             if (ITERATIONS_MODE) {
-                return iterations >= MAX_ITERATIONS;
+                return _iterations >= MAX_ITERATIONS;
             } else {
                 throw new RuntimeException("no maximums specified");
             }
@@ -276,39 +304,48 @@ public class Performance extends Object
      */
     public String getName()
     {
-        return name;
+        return _name;
     }
 
-    public SimpleNode getExpression()
+    public String getExpression()
     {
-        return expression;
+        return _expressionString;
     }
-    
+
     public Results testExpression(boolean compiled)
-        throws Exception
+            throws Exception
     {
         startTest();
         do {
-            if (compiled)
-                Ognl.getValue(compiledExpression.getAccessor(), context, root);
-            else
-                Ognl.getValue(expression, context, root);
+            if (!_isMvel)
+            {
+                if (compiled)
+                    Ognl.getValue(_compiledExpression.getAccessor(), _context, _root);
+                else
+                    Ognl.getValue(_expression, _context, _root);
+            } else
+            {
+                if (compiled)
+                    MVEL.executeExpression(_mvelCompiled, _root);
+                else
+                    MVEL.eval(_expressionString, _root);
+            }
         } while(!done());
         return endTest();
     }
 
     public Results testJava()
-        throws OgnlException
+            throws OgnlException
     {
         try {
-            return (Results) method.invoke(this, new Object[] {});
+            return (Results) _method.invoke(this, new Object[] {});
         } catch (Exception ex) {
-            throw new OgnlException("invoking java method '" + method.getName() + "'", ex);
+            throw new OgnlException("invoking java method '" + _method.getName() + "'", ex);
         }
     }
 
     public Results testConstantExpression()
-        throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
@@ -318,71 +355,71 @@ public class Performance extends Object
     }
 
     public Results testSinglePropertyExpression()
-        throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
-            root.getBean2();
+            _root.getBean2();
         } while(!done());
         return endTest();
     }
 
     public Results testPropertyNavigationExpression()
-        throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
-            root.getBean2().getBean3().getValue();
+            _root.getBean2().getBean3().getValue();
         } while(!done());
         return endTest();
     }
-    
+
     public Results testPropertyNavigationSetting()
-    throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
-            root.getBean2().getBean3().setNullValue("a value");
+            _root.getBean2().getBean3().setNullValue("a value");
         } while(!done());
         return endTest();
     }
-    
+
     public Results testPropertyNavigationAndComparisonExpression()
-        throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
-            boolean result = root.getBean2().getBean3().getValue() < 24;
+            boolean result = _root.getBean2().getBean3().getValue() < 24;
         } while(!done());
         return endTest();
     }
 
     public Results testIndexedPropertyNavigationExpression()
-        throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
-            root.getBean2().getBean3().getIndexedValue(25);
+            _root.getBean2().getBean3().getIndexedValue(25);
         } while(!done());
         return endTest();
     }
-    
+
     public Results testPropertyNavigationWithMapSetting()
-    throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
-            root.getBean2().getBean3().getMap().put("bam", "bam");
+            _root.getBean2().getBean3().getMap().put("bam", "bam");
         } while(!done());
         return endTest();
     }
-    
+
     public Results testPropertyNavigationWithMapExpression()
-        throws OgnlException
+            throws OgnlException
     {
         startTest();
         do {
-            root.getBean2().getBean3().getMap().get("foo");
+            _root.getBean2().getBean3().getMap().get("foo");
         } while(!done());
         return endTest();
     }
