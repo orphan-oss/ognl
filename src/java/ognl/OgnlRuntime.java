32d076d209c11da12bebe48d88321b961ac1b087
Yasser Zamani <yasserzamani@apache.org>
2019-09-13 14:49:44 +0430

    not fail on getDeclaredXXX when user has used a SecurityManager (#79)
    not fail on getDeclaredXXX when user has used a SecurityManager

--- src/java/ognl/OgnlRuntime.java
+++ src/java/ognl/OgnlRuntime.java
@@ -2174,7 +2174,12 @@ public class OgnlRuntime {
     }
 
     private static void collectMethods(Class c, Map result, boolean staticMethods) {
-        Method[] ma = c.getDeclaredMethods();
+        Method[] ma;
+        try {
+            ma = c.getDeclaredMethods();
+        } catch (SecurityException ignored) {
+            ma = c.getMethods();
+        }
         for (int i = 0, icount = ma.length; i < icount; i++)
         {
             if (c.isInterface())
@@ -2304,7 +2309,11 @@ public class OgnlRuntime {
                     Field fa[];
 
                     result = new HashMap(23);
-                    fa = targetClass.getDeclaredFields();
+                    try {
+                        fa = targetClass.getDeclaredFields();
+                    } catch (SecurityException ignored) {
+                        fa = targetClass.getFields();
+                    }
                     for (int i = 0; i < fa.length; i++) {
                         result.put(fa[i].getName(), fa[i]);
                     }
@@ -2602,7 +2611,12 @@ public class OgnlRuntime {
 
     private static void collectAccessors(Class c, String baseName, List result, boolean findSets)
     {
-        final Method[] methods = c.getDeclaredMethods();
+        Method[] methods;
+        try {
+            methods = c.getDeclaredMethods();
+        } catch (SecurityException ignored) {
+            methods = c.getMethods();
+        }
         for (int i = 0; i < methods.length; i++) {
             if (c.isInterface()) {
                 if (isDefaultMethod(methods[i]) || isNonDefaultPublicInterfaceMethod(methods[i])) {
