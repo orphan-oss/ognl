0012a89693ad5165b18ecbf318da73b2b7dc34d5
JCgH4164838Gh792C124B5 <43964333+JCgH4164838Gh792C124B5@users.noreply.github.com>
2019-10-05 16:06:46 -0400

    Cherry-Pick PR#69 into master.
    Resolved merge conflicts manually (included adding a logic block that was
    in 3.1.x and not in 3.2.x in OgnlRuntime near its merge conflict, and
    replacing usage of deprecated context usage in TestOgnlRuntime).
    
    Note: Was unable to determine why git mapped src/main/java/ognl/security
    package to src/etc/security/.  Was forced to re-add the three package
    members to their proper location and delete the unwanted src/etc/security
    elements.
    -----------
    invoke method in a fine grained java sandbox (#69)
    
    * invoke method in a fine grained java sandbox
    
    Applies a customized java SecurityManager on method invokes which disables ognl evaluations to do most top dangerous actions (execute,exit,thread access,... - see OgnlSecurityManager's overloaded methods). However, also honors previous security manager and policies if any set, as parent, and rolls back to them after ognl evaluation finished.
    
    * make JDK sandbox optional
    
    * fix and add test for concurrency performance and issues
    
    * fix a concurrency issue on install/uninstall sandbox & some improvements
    
    * move sandbox to dedicated package
    
    * secure sandbox itself against reflection from user method body execution
    
    * delete catch because like other places, user will handle exception
    
    * rename to "OgnlDefault..." to show they're overloadable/extensible
    
    * disable all actions e.g. file read/write
    
    Note: only package access is enabled because it seems very popular and also hard for user to sandbox
    
    * remove the enable/disable logic to JVM option
    
    * remove isInsideJDK
    
    isInsideJDK were designed to address java Inflation mechanism but now removed due to possible security flaws. Now user should use `-Dsun.reflect.inflationThreshold` or currently it's not possible to use sandbox in high-concurrent on one specific method.
    
    * honor user's security manager on enter
    
    * remove waste overrided methods
    
    because we want to block all permissions, overriding checkPermission is enough
    
    * provide a JVM wide singleton security manager with all permissions grant
    
    Now, OgnlSecurityManager in stack has an all permissions granted ProtectionDomain and so can simulate JDK itself SecurityManager exactly. For example doPrivileged works now and `-Dsun.reflect.inflationThreshold` isn't required any more :)
    
    Now, also the user's method will be executed with an all permissions denied ProtectionDomain and user must use doPrivileged if would like to do a sensitive action :)
    
    * create objects outside of sandbox to avoid web app class loader deny
    
    elsewhere OgnlSecurityManager denies access reading .class files when web app class loader tries to lazy load classes e.g. OgnlSandbox$1.class for inner anonymous class PrivilegedExceptionAction.
    
    * relax a bit with containers class loaders lazy class load
    
    TODO: might be risky but we have to - fix it if any POC discovered
    (cherry picked from commit e00101aa5f48933be42209fa77113177bf02f59d)
    
    # Conflicts:
    #	src/etc/security/OgnlSecurityManager.java
    #	src/etc/security/OgnlSecurityManagerFactory.java
    #	src/etc/security/UserMethod.java
    #	src/main/java/ognl/OgnlRuntime.java
    #	src/test/java/ognl/TestOgnlRuntime.java

--- src/test/java/org/ognl/test/objects/GenericService.java
+++ src/test/java/org/ognl/test/objects/GenericService.java
@@ -1,5 +1,8 @@
 package org.ognl.test.objects;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  *
  */
@@ -7,5 +10,19 @@ public interface GenericService {
 
     String getFullMessageFor(PersonGenericObject person, Object...arguments);
 
-    String getFullMessageFor(GameGenericObject game, Object...arguments);    
+    String getFullMessageFor(GameGenericObject game, Object...arguments);
+
+    void exec(long waitMilliseconds) throws InterruptedException, NoSuchMethodException, InvocationTargetException, IllegalAccessException;
+
+    void disableSandboxViaReflectionByProperty() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException;
+
+    void disableSandboxViaReflectionByField() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException;
+
+    void disableSandboxViaReflectionByMethod() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException;
+
+    void exit() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException;
+
+    int doNotPrivileged() throws IOException;
+
+    int doPrivileged() throws IOException;
 }
