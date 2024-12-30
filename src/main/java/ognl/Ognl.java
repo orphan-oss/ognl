026b794f7ca05612794feb6ca7d63f6bcd7591cc
Lukasz Lenart <lukasz.lenart@gmail.com>
2024-12-30 16:08:58 +0100

    Reverts call to addDefaultContext()

--- src/main/java/ognl/Ognl.java
+++ src/main/java/ognl/Ognl.java
@@ -398,22 +398,19 @@ public abstract class Ognl {
      * @throws OgnlException                    if there is a pathological environmental problem
      */
     public static Object getValue(Object tree, OgnlContext context, Object root, Class<?> resultType) throws OgnlException {
-        if (context.getRoot() == null) {
-            context.setRoot(root);
-        }
-
         Object result;
+        OgnlContext ognlContext = addDefaultContext(root, context);
 
         Node node = (Node) tree;
 
         if (node.getAccessor() != null) {
-            result = node.getAccessor().get(context, root);
+            result = node.getAccessor().get(ognlContext, root);
         } else {
-            result = node.getValue(context, root);
+            result = node.getValue(ognlContext, root);
         }
 
         if (resultType != null) {
-            result = getTypeConverter(context).convertValue(context, root, null, null, result, resultType);
+            result = getTypeConverter(ognlContext).convertValue(ognlContext, root, null, null, result, resultType);
         }
         return result;
     }
